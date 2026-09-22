6a1eb695cfa3d9a6a6853dcf01379d9cd9943036
Fahad <44726129+DaPotatoMan@users.noreply.github.com>
2026-09-22 06:28:07 +0600

    feat!: Merge icon fonts for improved tree shaking

--- test/solar_icons_test.dart
+++ test/solar_icons_test.dart
@@ -3,38 +3,33 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:solar_icons/solar_icons.dart';
 
 const fontPackage = 'solar_icons';
+const fontFamily = 'SolarIcons';
 
 void main() {
   group(SolarIconsBold, () {
     test('Test bold icons are generated', () {
-      const fontFamily = 'SolarIconsBold';
-      const icon =
-          IconData(0xe900, fontFamily: fontFamily, fontPackage: fontPackage);
+      const icon = IconData(0xe000, fontFamily: fontFamily, fontPackage: fontPackage);
 
       expect(icon, equals(SolarIconsBold.forward));
-      expect(icon.codePoint, 0xe900);
+      expect(icon.codePoint, 0xe000);
       expect(icon.fontFamily, SolarIconsBold.forward.fontFamily);
     });
   });
 
   group(SolarIconsOutline, () {
     test('Test that outline icons are generated', () {
-      const fontFamily = 'SolarIconsOutline';
-      const icon =
-          IconData(0xe900, fontFamily: fontFamily, fontPackage: fontPackage);
+      const icon = IconData(0xea00, fontFamily: fontFamily, fontPackage: fontPackage);
       expect(icon, equals(SolarIconsOutline.forward));
-      expect(icon.codePoint, 0xe900);
+      expect(icon.codePoint, 0xea00);
       expect(icon.fontFamily, SolarIconsOutline.forward.fontFamily);
     });
   });
 
   group(SolarIconsBroken, () {
     test('Test that broken icons are generated', () {
-      const fontFamily = 'SolarIconsBroken';
-      const icon =
-          IconData(0xe900, fontFamily: fontFamily, fontPackage: fontPackage);
+      const icon = IconData(0xe500, fontFamily: fontFamily, fontPackage: fontPackage);
       expect(icon, equals(SolarIconsBroken.multipleForwardLeft));
-      expect(icon.codePoint, 0xe900);
+      expect(icon.codePoint, 0xe500);
       expect(icon.fontFamily, SolarIconsBroken.multipleForwardLeft.fontFamily);
     });
   });
